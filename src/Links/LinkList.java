bff38f3901f465c8f5e2a2491f674b7156bbb561
“cn.qjh <1968747146@qq.com>
2020-02-22 17:31:14 +0800

    单链表

---
+++ src/Links/LinkList.java
@@ -0,0 +1,70 @@
+package Links;
+
+/**
+ * @Author Mu_Hui
+ * @Date 2020-2-18 18:17:26
+ * @Name 单链表
+ */
+public class LinkList {
+    public static void main(String[] args) {
+        linkList linklist = new linkList();
+        linklist.InsertFirst(1, 2);
+        linklist.InsertFirst(2, 22);
+        linklist.InsertFirst(3,2);
+        linklist.InsertFirst(4,23);
+        linklist.InsertFirst(5,24);
+        linklist.DisplayLinkList();
+        linklist.deleteFirst();
+        linklist.deleteFirst();
+        linklist.DisplayLinkList();
+    }
+
+    public static class Link {
+        public int iData;
+        public double dData;
+        public Link next;
+
+        public Link(int id, double dd) {
+            iData = id;
+            dData = dd;
+        }
+
+        public void DisplayLink() {
+            System.out.println("{" + iData + "," + dData + "}");
+        }
+    }
+
+    public static class linkList {
+        private Link first;
+
+        public linkList() {
+            first = null;
+        }
+
+        public boolean IsEmpty() {
+            return (first == null);
+        }
+
+        public void InsertFirst(int id, double dd) {
+            Link newlink = new Link(id, dd);
+            newlink.next=first;
+            first = newlink;
+        }
+
+        public Link deleteFirst() {
+            Link temp = first;
+            first = first.next;
+            return temp;
+        }
+
+        public void DisplayLinkList() {
+            System.out.println("List(first-->last)：");
+            Link current = first;
+            while (current != null) {
+                current.DisplayLink();
+                current = current.next;
+            }
+            System.out.println(" ");
+        }
+    }
+}
